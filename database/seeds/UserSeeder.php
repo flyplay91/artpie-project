b2f517b86bc0bb12e34a3d1ae2eb5a168d35ea4f
flyplay91 <flyplay91@gmail.com>
2021-12-24 11:22:19 -0500

    fix user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,6 +15,7 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'superadmin',
             'email' => 'ro@xisensoft.com',
+            'address_1' => 'Address 1',
             'password' => bcrypt('admin1234'),
             'role' => 1
         ]);
