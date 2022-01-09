ef5b90bc37567c7f0c4604266c1da22ba66b4ec4
flyplay91 <flyplay91@gmail.com>
2022-01-09 03:53:34 -0500

    fixed my gallery page style

--- resources/views/front/pages/account/deposits.blade.php
+++ resources/views/front/pages/account/deposits.blade.php
@@ -2,35 +2,33 @@
 
 @section('content')
   <div class="account-deposits-page">
-    <div class="container">
-      <div class="block-admin-deposit">
-        <h2>{{ __('messages.deposit_funds') }}</h2>
-        <form class="deposit-form" method="post" action="{{ route('process-deposit') }}">
-          @csrf
+    
+    <h2>{{ __('messages.deposit_funds') }}</h2>
+    <form class="deposit-form" method="post" action="{{ route('process-deposit') }}">
+      @csrf
 
-          <div class="deposit-form__inner flex aic jcb">
-            <img src="/images/wechat-icon.png" />
-          
-            <div class="deposit-form-price flex-column flex aic jcb">
-              
-              <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">{{ __('messages.price_2') }} (USD)</span>
-                </div>
-                <input type="number" name="amount" class="form-control">
-              </div>
-                
-              <div class="flex jcb w-100 mt-2">
-                <button class="btn btn-grey" type="submit">{{ __('messages.send_request') }}</button>
-                <a href="/my-gallery" class="btn btn-grey">{{ __('messages.cancel') }}</a>
-              </div>
-            </div>
-            
-          </div>
-        </form>
-      </div>
-
+      <div class="deposit-form__inner flex aie jcb">
+        <img src="/images/wechat-icon.png" />
       
-    </div>
+        <div class="deposit-form-price flex-column flex aic jcb">
+          
+          <div class="flex jcb w-100 mt-2">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                  <span class="input-group-text">{{ __('messages.price_2') }} (USD)</span>
+              </div>
+              <input type="number" name="amount" class="form-control">
+            </div>
+            <button class="btn btn-grey" type="submit">{{ __('messages.send_request') }}</button>
+          </div>
+            
+          <div class="w-100 mt-3 text-right">
+            <a href="/my-gallery" class="btn btn-grey">{{ __('messages.cancel') }}</a>
+          </div>
+        </div>
+        
+      </div>
+    </form>
+
   </div>
 @endsection
